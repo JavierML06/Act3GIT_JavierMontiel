22b7244d51f285c14472acb3b5577077f50e4d14
Javier Montiel Lopez <jmonlop0804@g.educaand.es>
2024-11-20 10:00:27 +0100

    Mod Gen 30 nums

--- src/prueba1/numerosaleatorios.java
+++ src/prueba1/numerosaleatorios.java
@@ -5,7 +5,7 @@ public class numerosaleatorios {
 	public static void main(String[] args) {
 	
 		Random rand = new Random();
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 30; i++) {
             System.out.println(rand.nextInt(10) + 1);
         }
     }
